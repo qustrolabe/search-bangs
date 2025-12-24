5bdfabd314507382f4c883a91de0bf21735ef367
qustrolabe <qustrolabe@gmail.com>
2025-12-24 06:10:03 +0200

    Formatting

--- src/logic.ts
+++ src/logic.ts
@@ -1,4 +1,4 @@
-import { bangs } from "./bang";
+import { bangs } from "./bang.ts";
 
 export function getRedirectUrl(query: string | null): string | null {
     if (!query) return null;
@@ -8,19 +8,21 @@ export function getRedirectUrl(query: string | null): string | null {
 
     // Default to "g" (Google) if implicit, assuming that's what was in main.ts
     const defaultBang = bangs.find((b) => b.t === "g");
-    const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
+    const selectedBang = bangs.find((b) => b.t === bangCandidate) ??
+        defaultBang;
 
     // Remove the first bang from the query
     const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
 
     // If the query is just `!gh`, use `github.com` instead of `github.com/search?q=`
-    if (cleanQuery === "")
+    if (cleanQuery === "") {
         return selectedBang ? `https://${selectedBang.d}` : null;
+    }
 
     const searchUrl = selectedBang?.u.replace(
         "{{{s}}}",
         // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
-        encodeURIComponent(cleanQuery).replace(/%2F/g, "/")
+        encodeURIComponent(cleanQuery).replace(/%2F/g, "/"),
     );
 
     return searchUrl ?? null;
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: '/search-bangs/',
+  base: "/search-bangs/",
   plugins: [
     VitePWA({
       strategies: "injectManifest", // Enable custom SW
-      srcDir: "src",                // Look in src folder
-      filename: "sw.ts",            // For this file
+      srcDir: "src", // Look in src folder
+      filename: "sw.ts", // For this file
       manifest: {
         theme_color: "#111",
         start_url: "/search-bangs/",
@@ -16,7 +16,7 @@ export default defineConfig({
       devOptions: {
         enabled: true, // Allows testing SW in dev mode
         type: "module",
-      }
+      },
     }),
   ],
 });
